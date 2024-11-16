625f35beb9f688d6612a3e3cc7597261be66c6f4
vigneswaranbranavan <vigneswaranbranavan@gmail.com>
2024-11-16 12:33:39 +0530

    Add component structure

---
+++ src/app/Schedule/Timetable/timetable-management/timetable-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetable-management.component.html',
+  styleUrl: './timetable-management.component.css'
+})
+export class TimetableManagementComponent {
+
+}
---
+++ src/app/Schedule/Timetable/timetable-view/timetable-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetable-view.component.html',
+  styleUrl: './timetable-view.component.css'
+})
+export class TimetableViewComponent {
+
+}
