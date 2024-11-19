c098ff01649a697c319b4fbeee0b04e02867500f
vigneswaranbranavan <vigneswaranbranavan@gmail.com>
2024-11-19 16:42:19 +0530

    Restructured the folder and component organization.

---
+++ src/app/Staff/manage-timetable/manage-timetable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-timetable',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-timetable.component.html',
+  styleUrl: './manage-timetable.component.css'
+})
+export class ManageTimetableComponent {
+
+}
---
+++ src/app/Staff/mark-attendance/mark-attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mark-attendance',
+  standalone: true,
+  imports: [],
+  templateUrl: './mark-attendance.component.html',
+  styleUrl: './mark-attendance.component.css'
+})
+export class MarkAttendanceComponent {
+
+}
