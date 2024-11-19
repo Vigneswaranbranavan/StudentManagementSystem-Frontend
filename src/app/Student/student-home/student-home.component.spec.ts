82248cfbd06648203a9ec5575476c51e7fce571a
ut01265 <ut01265@UNICOMTIC24>
2024-11-19 15:29:13 +0530

    Create StudentFolder And Implement The Attendance, Timetable, Notification, Profile And FeedBack Components

--- src/app/Student/student-home/student-home.component.spec.ts
+++ src/app/Student/student-home/student-home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidebarComponent } from './sidebar.component';
+import { StudentHomeComponent } from './student-home.component';
 
-describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+describe('StudentHomeComponent', () => {
+  let component: StudentHomeComponent;
+  let fixture: ComponentFixture<StudentHomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
+      imports: [StudentHomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SidebarComponent);
+    fixture = TestBed.createComponent(StudentHomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
