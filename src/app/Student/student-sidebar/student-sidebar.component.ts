031a11cb13e3515ff7d7e9f334f4a279130f2df4
ut01107 <ut01107@UNICOMTIC11>
2024-11-20 15:29:37 +0530

    Students Attendence Component Added

--- src/app/Student/student-sidebar/student-sidebar.component.ts
+++ src/app/Student/student-sidebar/student-sidebar.component.ts
@@ -1,18 +1,17 @@
 import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { StudentHomeComponent } from '../student-home/student-home.component';
 
 
 @Component({
   selector: 'app-student-sidebar',
   standalone: true,
-  imports: [NgClass, RouterModule,StudentHomeComponent],
+  imports: [NgClass, RouterModule],
   templateUrl: './student-sidebar.component.html',
   styleUrl: './student-sidebar.component.css'
 })
 export class StudentSidebarComponent {
-  isOpen = false;
+  isOpen = true;
 
   toggleSidebar() {
     this.isOpen = !this.isOpen;
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,18 @@ import { Routes } from '@angular/router';
 import { StudentAttendanceComponent } from './Student/student-attendance/student-attendance.component';
 import { StudentFeedbackComponent } from './Student/student-feedback/student-feedback.component';
 import { StudentNotificationComponent } from './Student/student-notification/student-notification.component';
-import { StudentProfileComponent } from './User-Management/student-profile/student-profile.component';
 import { StudentTimetableComponent } from './Student/student-timetable/student-timetable.component';
 import { StudentHomeComponent } from './Student/student-home/student-home.component';
 import { LoginComponent } from './Auth/login/login.component';
-import { RegisterUserComponent } from './Administrator/register-user/register-user.component';
+import { EditProfileComponent } from './Student/edit-profile/edit-profile.component';
 
 export const routes: Routes = [
-    {path : 'home', component: StudentHomeComponent },
+    {path : '', component: StudentHomeComponent },
     {path : 'attendance', component: StudentAttendanceComponent },
     {path : 'feedback', component: StudentFeedbackComponent },
     {path : 'notification', component: StudentNotificationComponent },
-    {path : 'profile', component: StudentProfileComponent },
     {path : 'timetable', component: StudentTimetableComponent },
-    {path : 'register', component: RegisterUserComponent },
+    {path : 'edit-profile', component: EditProfileComponent },
 
     
 
