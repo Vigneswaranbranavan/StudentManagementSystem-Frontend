54e2db6ffcd70fcbc44a28af8e3fe2b8f978cfdc
Selvachandran R. <UT01107@unicomtic.com>
2024-11-22 14:27:18 +0530

    Add CRUD methods to StaffProfileService for managing Staff Profile

---
+++ src/app/Service/Models/models.ts
@@ -0,0 +1,24 @@
+export interface staff {
+    id: number;
+    name:string;
+    email: string;
+    phone: string;
+    password: string;
+  }
+
+  export interface teacher {
+    id: number;
+    name:string;
+    email: string;
+    phone: string;
+    password: string;
+  }
+
+  export interface student {
+    id: number;
+    name:string;
+    email: string;
+    phone: string;
+    password: string;
+  }
+
---
+++ src/app/Service/Profile/staff-profile.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { staff } from '../Models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaffProfileService {
+
+  url ='http://localhost:5101/api/Staff/Staff';
+
+
+  constructor(private http:HttpClient) { }
+
+  
+  createStaff(staff:staff){
+    return this.http.post(this.url,staff)
+  }
+  getStaffs(){
+    return this.http.get<any[]>(this.url)
+  }
+  getStaff(staffId:number){
+    return this.http.get<staff>(this.url+'/'+staffId)
+  }
+  updateStaff(staff:staff ,staffId:number){
+    return this.http.put(this.url+'/'+staffId,staff)
+  }
+  deleteStaff(staffid: number){
+    return this.http.delete(this.url+'/'+staffid)
+  }
+
+
+}
