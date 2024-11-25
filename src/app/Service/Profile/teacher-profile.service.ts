4b08f95a4851fa5365e8492d42809bbea6d97d6e
ut01107 <ut01107@UNICOMTIC11>
2024-11-25 15:29:43 +0530

    Profile Services for Teachers Added

--- src/app/Service/Profile/teacher-profile.service.ts
+++ src/app/Service/Profile/teacher-profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { student } from '../Models/model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherProfileService {
+  url = 'http://localhost:5101/api/Teacher/Teacher';
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) {}
+
+  // getStudentProfile(studentId: number): Observable<any> {
+  //   return this.http.get(`${this.url}/${studentId}`);
+  // }
+
+  getTeacher(teacherId: string) {
+    return this.http.get<student>(`${this.url}/${teacherId}`);
+  }
+
+  updateStudent(teacher: student, teacherId: number) {
+    return this.http.put(`${this.url}/${teacherId}`, teacher);
+  }}
