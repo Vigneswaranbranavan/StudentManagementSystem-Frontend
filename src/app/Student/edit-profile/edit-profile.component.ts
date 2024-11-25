1017b821ca3e9cb8d5dde2520fec7e3bc5ed2bce
ut01107 <ut01107@UNICOMTIC11>
2024-11-25 14:57:58 +0530

    Teacher Timetable and Profile Services Added

--- src/app/Student/edit-profile/edit-profile.component.ts
+++ src/app/Student/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { student } from '../../Service/Models/model';
+import { StudentProfileService } from '../../Service/Profile/student-profile.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,6 +10,46 @@ import { Component } from '@angular/core';
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css'
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit {
 
+  studentid: string;
+  
+
+
+
+
+
+  student: student = {
+    id: '',
+    name: '',
+    phone: '',
+    enrollmentDate: '',
+    classID: ''
+  };
+
+  constructor(
+    private studentProfileService: StudentProfileService,
+    private route: ActivatedRoute
+  ) {
+    const tid = this.route.snapshot.paramMap.get('id');
+    this.studentid = String(tid);
+  }
+
+  ngOnInit(): void {
+    if (this.studentid) {
+      this.getStudentInfo(this.studentid);  // Fetch student data using studentid
+    }
+  }
+
+  getStudentInfo(studentid: string) {
+    this.studentProfileService.getStudent(studentid).subscribe(
+      (data) => {
+        this.student = data;
+      },
+      (error) => {
+        console.error('Error fetching student:', error);
+        // Optionally, show user-friendly error messages (e.g., using toastr)
+      }
+    );
+  }
 }
