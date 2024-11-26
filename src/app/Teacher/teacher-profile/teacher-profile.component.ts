ad0fc4b3fba0bf0053cc1a2d0a1c0adadb1ddcc7
Selvachandran R. <UT01107@unicomtic.com>
2024-11-26 14:03:33 +0530

    Teacher Profile View was Amended

--- src/app/Teacher/teacher-profile/teacher-profile.component.ts
+++ src/app/Teacher/teacher-profile/teacher-profile.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit, reflectComponentType } from '@angular/core';
+import { Component, OnInit, reflectComponentType } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { TeacherProfileService } from '../../Service/Profile/teacher-profile.service';
 import { student, teacher } from '../../Service/Models/model';
@@ -12,11 +12,47 @@ import { NgIf } from '@angular/common';
   selector: 'app-teacher-profile',
   templateUrl: './teacher-profile.component.html',
   styleUrls: ['./teacher-profile.component.css'],
-  imports: [RouterModule, HttpClientModule, ReactiveFormsModule, NgIf],
-  providers: [TeacherProfileService]
+  imports: [RouterModule, HttpClientModule, ReactiveFormsModule,NgIf],
+  providers:[TeacherProfileService]
 
 })
-export class TeacherProfileComponent {
+export class TeacherProfileComponent implements OnInit {
 
 
+  teacherid: string;
+  
+
+  Teacher: teacher = {
+    email: '',
+    id: '',
+    name: '',
+    phone: '',
+    subjectID: '',
+  };
+
+  constructor(
+    private TeacherProfileService: TeacherProfileService,
+    private route: ActivatedRoute
+  ) {
+    const tid = this.route.snapshot.paramMap.get('id');
+    this.teacherid = String(tid);
+  }
+
+  ngOnInit(): void {
+    if (this.teacherid) {
+      this.getTeacherInfo(this.teacherid);  // Fetch student data using studentid
+    }
+  }
+
+  getTeacherInfo(teacherid: string) {
+    this.TeacherProfileService.getTeacher(teacherid).subscribe(
+      (data) => {
+        this.Teacher = data;
+      },
+      (error) => {
+        console.error('Error fetching Teacher:', error);
+        // Optionally, show user-friendly error messages (e.g., using toastr)
+      }
+    );
+  }
 }
